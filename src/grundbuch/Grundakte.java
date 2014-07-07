d5dd87c78e3788444238fcd08c0ad503ae8f9dd6
sebastian <sebastian2406@t-online.de>
2014-07-07 23:24:00 +0200

    angefangen Grundbuch zu implementieren

--- src/grundbuch/Grundakte.java
+++ src/grundbuch/Grundakte.java
@@ -1,5 +1,30 @@
 package grundbuch;
 
-public class Grundakte {
+import java.util.Date;
 
+public class Grundakte {
+	private Date eingangssatum;
+	private Date bearbeitungsdatum;
+	private Grundbuchblatt grundbuchBlatt;
+	
+	public void create(Date eingangsdatum, Grundbuchblatt grundbuchBatt){
+		this.eingangssatum = eingangsdatum;
+		this.grundbuchBlatt = grundbuchBatt;
+	}
+	
+	public void change(Date bearbeitungsdatum){
+		this.bearbeitungsdatum = bearbeitungsdatum;
+	}
+	
+	public Date getBearbeitungsdatum() {
+		return bearbeitungsdatum;
+	}
+	
+	public Date getEingangssatum() {
+		return eingangssatum;
+	}
+	
+	public Grundbuchblatt getGrundbuchBlatt() {
+		return grundbuchBlatt;
+	}
 }
--- src/grundbuch/Grundbuch.java
+++ src/grundbuch/Grundbuch.java
@@ -1,5 +1,17 @@
 package grundbuch;
 
-public class Grundbuch {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Grundbuch {
+	List<Grundbuchblatt> blaetter = new ArrayList<Grundbuchblatt>();
+	
+	public void createNewBlatt(){
+		//TODO: Liste Implementieren
+	}
+	
+	public boolean blattExists(){
+		//TODO: Liste Implementieren
+		return false;
+	}
 }
