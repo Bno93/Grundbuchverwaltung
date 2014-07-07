e81a03a48a4d3f050ce68f07671c0fa3590392b2
sebastian <sebastian2406@t-online.de>
2014-07-07 23:02:22 +0200

    Login GUI Fertig gestellt! Grundbuchverwaltung öffnet Login!
    TODO: Login prüfung muss implementiert werden. --> Datenbank Klassen
    benötigt!!!

--- src/main/GrundbuchVerwaltung.java
+++ src/main/GrundbuchVerwaltung.java
@@ -1,5 +1,8 @@
 package main;
 
+import javax.swing.JOptionPane;
+
+import rieger.ui.MessageHandler;
 import ui.LoginGUI;
 import ui.MainGUI;
 
@@ -10,28 +13,56 @@ import ui.MainGUI;
  *
  */
 public class GrundbuchVerwaltung {
+	
+	static MessageHandler messageHandler;
 
 	/**
 	 * Starts the {@link LoginGUI}.
 	 * @return <code>true</code> after a successful login otherwise <code>false</code>.
 	 */
-	public static boolean login(){
+	public static int login(){
 		System.out.println("===Login GUI START==");
 		LoginGUI gui = new LoginGUI();
-		gui.startGui();
-		return false;
+		return gui.startGui();
 	}
 
 	/**
-	 * Main method which initialize the {@link LoginGUI} and the {@link MainGUI}.
+	 * Main method which initialize the {@link LoginGUI} and the {@link MainGUI} if the Login is correct.
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		System.out.println("===Start Grundbuchverwaltung");
+		
+		int whichButtonIsPressedInLoginGUI;
+		boolean loginCorrect;
+		
+		//wiederhole Login bis richtig!
 		do{
-			System.out.println("===Login not successful===");
-		}while(login()==false);
+		whichButtonIsPressedInLoginGUI = login();
+		
+		//brich ab wenn User das wünscht
+		if(whichButtonIsPressedInLoginGUI==-1||whichButtonIsPressedInLoginGUI==1)
+			break;
+		loginCorrect = !loginCorrect();
+		
+		//prüfe ob zugang genehmigt mit loginCorrect()
+		if(loginCorrect){
+			MessageHandler.showErrorMessage(null, "Login nicht Korrekt");
+		}else{
+			System.out.println("===Starte Main GUI===");
+		}
+		}while(loginCorrect);
 
+	}
+	/**
+	 * Check all Inputs if you can Login.
+	 * @return <code>true</code> if access graduate or <code>false</code> if access denied.
+	 */
+	public static boolean loginCorrect(){
+		//TODO: Test Login
+		System.out.println("Login muss noch getestet werden");
+		return false;
 	}
 
 }
--- src/ui/LoginGUI.java
+++ src/ui/LoginGUI.java
@@ -17,32 +17,36 @@ import rieger.ui.FirstClickEraser;
  */
 public class LoginGUI {
 	JPanel logPanel = new JPanel();
+	
+	//create JLabel
+	JLabel hostLabel = new JLabel("Host Name:");
+	JLabel portLabel = new JLabel("Port Nummer:");
+	JLabel DatabaseNameLabel = new JLabel("Datenbank:");
+	JLabel userNameLabel = new JLabel("User Name:");
+	JLabel passwordLabel = new JLabel("Passwort:");
+	
+	//create Textfelder
+	JTextField hostField = new JTextField();
+	JTextField portField = new JTextField();
+	JTextField DatabaseNameField = new JTextField();
+	JTextField userNameField = new JTextField();
+	JPasswordField passwordField = new JPasswordField();
+	
 	/**
 	 * Create the Login-GUI for the User.
 	 */
 	public LoginGUI() {
 
-		//create JLabel
-		JLabel hostLabel = new JLabel("Host Name:");
-		JLabel portLabel = new JLabel("Port Nummer:");
-		JLabel DatabaseNameLabel = new JLabel("Datenbank:");
-		JLabel userNameLabel = new JLabel("User Name:");
-		JLabel passwordLabel = new JLabel("Passwort:");
-
-		//create Textfelder
-		JTextField hostField = new JTextField();
-		JTextField portField = new JTextField();
-		JTextField DatabaseNameField = new JTextField("Datenbank");
-		JTextField userNameField = new JTextField("UserName");
-		JPasswordField passwordField = new JPasswordField("Passwort");
 
 		//add FirstClickEraser
-		FirstClickEraser.textFieldHint(hostField, "Hostname");
-		FirstClickEraser.textFieldHint(portField, "Port");
-		FirstClickEraser.textFieldHint(DatabaseNameField, "Datenbank");
-		FirstClickEraser.textFieldHint(userNameField, "UserName");
-		FirstClickEraser.textFieldHint(passwordField, "Passwort");
-
+//		FirstClickEraser fce = new FirstClickEraser();
+//		
+//		fce.addHint(hostField, "Hostname");
+//		fce.addHint(portField, "Port");
+//		fce.addHint(DatabaseNameField, "Datenbank");
+//		fce.addHint(userNameField, "Username");
+//		fce.addHint(passwordField, "Passwort");
+		
 		//add Labels and Fields to JPanel
 		this.logPanel.add(hostLabel);
 		this.logPanel.add(hostField);
@@ -61,10 +65,51 @@ public class LoginGUI {
 
 	}
 	/**
-	 *
-	 * @return
+	 * Show the Login-Window
+	 * @return 0 if Login is pressed. <p>1 if Abbrechen is pressed</p> <p>-1 if the user close the Window</p>
 	 */
 	public int startGui(){
 		return JOptionPane.showOptionDialog(null, this.logPanel, "Grundbuchverwaltung Login", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"Login", "Abbrechen"}, "Login");
+	}
+	
+	/**
+	 * Get the last DatabaseName from {@link JTextField}
+	 * @return DatabaseName
+	 */
+	public String getDatabaseNameField() {
+		return DatabaseNameField.getText();
+	}
+	
+	/**
+	 * Get the last HostName from {@link JTextField}
+	 * @return HostName
+	 */
+	public String getHostField() {
+		return hostField.getText();
+	}
+	
+	/**
+	 * Get the last Password from {@link JTextField}
+	 * @return password
+	 */
+	public char[] getPasswordField() {
+		return passwordField.getPassword();
+	}
+	
+	/**
+	 * Get the last Port from {@link JTextField}
+	 * @return Port
+	 */
+	public int getPortField() {
+		//TODO: Test ob Parse möglich!!!!!
+		return Integer.parseInt(portField.getText());
+	}
+	
+	/**
+	 * Get the last UserName from {@link JTextField}
+	 * @return UserName
+	 */
+	public String getUserNameField() {
+		return userNameField.getText();
 	}
 }
